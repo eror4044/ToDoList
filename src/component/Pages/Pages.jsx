9e4b7f3f88f8883cab18916229d05a57eea7ef5c
Egor <msubzero2001@gmail.com>
2024-05-16 22:46:21 +0300

    sorry

--- src/component/Pages/Pages.jsx
+++ src/component/Pages/Pages.jsx
@@ -1,28 +1,36 @@
 import Home from "../Home";
 import ToDoList from "../ToDoList/ToDoList";
 import styled from "styled-components";
-import { NavLink,Routes,Route } from "react-router-dom";
+import { NavLink, Routes, Route } from "react-router-dom";
 import LoginForm from "../LoginForm/LoginForm";
 import { useSelector } from "react-redux";
 import User from "../LoginForm/User";
+import css from "./pages.module.css";
 
 const StyledLink = styled(NavLink)`
   color: black;
 
   &.active {
-    color: orange;
+    color: white;
+    
+  }
+  &.active::after {
+    opacity: 1;
+    
   }
 `;
 
 const Pages = () => {
-  const isLogged = useSelector((state) => state.login.isLoggedIn)
+  const isLogged = useSelector((state) => state.login.isLoggedIn);
   return (
-    <div className="Papa">
-      <nav className="css.nav">
-        <StyledLink to="/">Home</StyledLink>
-        <StyledLink to="/todolist">ToDoList</StyledLink>
-        <StyledLink to="/loginform">LoginForm</StyledLink>
-      </nav>
+    <div className={css.page_box}>
+    
+        <nav className={css.nav_box}>
+          <StyledLink to="/" className={css.nav_link}>Home</StyledLink>
+          <StyledLink to="/todolist"className={css.nav_link}>ToDoList</StyledLink>
+          <StyledLink to="/loginform"className={css.nav_link}>LoginForm</StyledLink>
+        </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/todolist" element={<ToDoList />} />
